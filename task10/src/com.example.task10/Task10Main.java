18bdc11d6ea4fc2b8cdff369ae19bdaa3c94ce23
leclud <negativchikxxl@gmail.com>
2024-09-18 17:49:20 +0500

    10 task

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,16 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minimum = arr[0];
+        int minIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= minimum) {
+                minimum = arr[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
     }
 
 }
